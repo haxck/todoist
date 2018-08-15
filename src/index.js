7bd77370f28f745251075884482a56661f9bdbb2
haxck <haxck.x@gmail.com>
2018-08-15 18:24:12 +0800

    style(touch): hammerjs

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
+import touch from './touch'
 
 
 if ("serviceWorker" in navigator) {
@@ -7,43 +8,6 @@ if ("serviceWorker" in navigator) {
 }
 
 
-function vueTouch(el, type, binding) {
-  this.el = el;
-  this.type = type;
-  this.binding = binding;
-  var hammertime = new Hammer(this.el);
-  hammertime.on(this.type, this.binding.value);
-};
-Vue.directive("tap", {
-  bind: function (el, binding) {
-    new vueTouch(el, "tap", binding);
-  }
-});
-Vue.directive("swipeleft", {
-  bind: function (el, binding) {
-    new vueTouch(el, "swipeleft", binding);
-  }
-});
-Vue.directive("swiperight", {
-  bind: function (el, binding) {
-    new vueTouch(el, "swiperight", binding);
-  }
-});
-Vue.directive("press", {
-  bind: function (el, binding) {
-    new vueTouch(el, "press", binding);
-  }
-})
-Vue.directive("pan", {
-  bind: function (el, binding) {
-    new vueTouch(el, "pan", binding);
-  }
-})
-Vue.directive("panend", {
-  bind: function (el, binding) {
-    new vueTouch(el, "panend", binding);
-  }
-})
 
 /* eslint-disable no-new */
 new Vue({
